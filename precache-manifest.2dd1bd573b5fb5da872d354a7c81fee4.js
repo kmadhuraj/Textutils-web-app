eac4dc4ce2a82fbda855ec162302205ed32b0527
kmadhuraj <madhurajmadu693@gmail.com>
2023-11-16 10:17:57 +0530

    Updates

---
+++ precache-manifest.2dd1bd573b5fb5da872d354a7c81fee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f51c1edf04cb359739594bfc9db409",
+    "url": "/Textutils-web-app/index.html"
+  },
+  {
+    "revision": "33b7c8c9ef3e98538526",
+    "url": "/Textutils-web-app/static/css/main.af8645d3.chunk.css"
+  },
+  {
+    "revision": "259afa2894b45131e436",
+    "url": "/Textutils-web-app/static/js/2.e33bfa39.chunk.js"
+  },
+  {
+    "revision": "1f152a4ac5c8a24a9d1f415aafdcdff4",
+    "url": "/Textutils-web-app/static/js/2.e33bfa39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b7c8c9ef3e98538526",
+    "url": "/Textutils-web-app/static/js/main.a1a5ac7c.chunk.js"
+  },
+  {
+    "revision": "559c720fe583f6c9d08a",
+    "url": "/Textutils-web-app/static/js/runtime-main.6cb048bc.js"
+  }
+]);
